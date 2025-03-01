bba88c37236896dddd820f13c75d2716597688a3
gabriel <ga.lima2205@gmail.com>
2025-02-28 23:21:23 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link"
 import ProjectCard from "./_components/project-card"
 import TechStack from "./_components/tech-stack"
 import { TextAnimate } from "@/components/magicui/text-animate"
-import { MarqueeDemo } from "./_components/review-card"
 
 export default function Page() {
   return (
@@ -93,17 +92,11 @@ export default function Page() {
                 isDeploy={true}
                 deploy="https://consultawebcep.vercel.app/"
               />
+
             </div>
           </div>
         </section>
-        <section id="reviews" className="py-12 md:py-24 lg:py-32">
-          <div className="container px-4 md:px-6">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">
-              Reviews
-            </h2>
-            <MarqueeDemo />
-          </div>
-        </section>
+
         <section className="py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">
